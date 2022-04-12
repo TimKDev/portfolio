30c2b635e9cb2e308f14ca278e6a82a86580b918
TimKDev <99671546+TimKDev@users.noreply.github.com>
2022-04-12 14:07:20 +0200

    added contact-me section

--- src/app/contact-me-section/contact-me-section.component.ts
+++ src/app/contact-me-section/contact-me-section.component.ts
@@ -1,15 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me-section',
   templateUrl: './contact-me-section.component.html',
   styleUrls: ['./contact-me-section.component.scss']
 })
-export class ContactMeSectionComponent implements OnInit {
+export class ContactMeSectionComponent {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
-  ngOnInit(): void {
+  checkoutForm = this.formBuilder.group({
+    title: '',
+    email: '',
+    description: ''
+  });
+  // Das checkoutForm wird dann vom im HTML der Komponente als Property Binding für das 
+  // Form Tag Attribut formGroup verwendet.
+
+  onSubmit(): void {
+    console.log('Email was sent with ', this.checkoutForm.value);
+    this.checkoutForm.reset();
   }
+
 
 }
